fc810438aa525a77cf076cb57352209468cccf4d
vitor-hc <vitor_hc@hotmail.com.br>
2021-07-04 22:17:02 -0300

    Controle de estoque v1.0

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Green Town - O Hortifruti mais perto de você </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
